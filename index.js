4d092d886e3196dcb93f9e86a2a13e8acf2e265f
Tyler <t.woods95@aol.com>
2023-06-10 20:02:19 -0400

    Added shapes js file and classes, exported file to index.

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const {Triangle, Circle, Square} = require("./lib/shapes");
 const questions = [{
     type: "input",
-    name: "logo",
-    message: "what type of logo?"
+    name: "text",
+    message: "what type of text?",
+    validate: (input) => {
+        // console.log(input)
+        if (input.length <= 3){
+            return true
+        }
+    
+        else {
+            return "Input must be 3 characters or less!"
+        }
+    }
+},
+{
+    type: "input",
+    name: "text_color",
+    message: "what type of text color?"
+},
+{
+    type: "list",
+    name: "shape",
+    message: "what type of shape?",
+    choices: ['circle', 'triangle', 'square']
+},
+{
+    type: "input",
+    name: "shape_color",
+    message: "what type of shape color?"
 }]
 
 function init(){
-    inquirer.prompt(questions).then((answers) => writeFile(answers))
+    inquirer.prompt(questions).then((answers) => {
+        let shape;
+        switch (answers.shape){
+            case "circle":
+                shape = new Circle();
+                break;
+            case "triangle":
+                shape = new Triangle();
+                break;
+            case "square":
+                shape = new Square();
+                break;
+        }
+        shape.setColor(`${answers.shape_color}`);
+        console.log(answers)
+        console.log(shape)
+        // writeFile(answers)
+    })
+    
+    
 }
 init();
 
 function writeFile(answers){
-    let logoAnswer = answers.logo;
-    fs.writeFile("./samples/sample.svg", logoAnswer, (err) => err ? console.error(err) : console.log("success"))
+    let textAnswer = answers.text;
+    fs.writeFile("./samples/sample.svg", textAnswer, (err) => err ? console.error(err) : console.log("success"))
+
 };
